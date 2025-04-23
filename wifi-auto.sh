6bfa3eb5933ee8e7ce39243f2f0b11bd45ddae24
Kenteni <josekenteniab@gmail.com>
2025-04-23 20:05:42 -0300

    FixToMe

--- wifi-auto.sh
+++ wifi-auto.sh
@@ -25,20 +25,13 @@ echo """
 
 # CONFIGURAÇÃO - Edite esses dois:
 SSID="NOME_DA_REDE"
-PASSWORD="SENHA_DA_REDE"
 
-WPA_CONF="/etc/wpa_supplicant/wpa_supplicant-wlan1.conf"
 
 echo "[*] Ativando interface wlan1..."
 ip link set wlan1 up
 
-echo "[*] Gerando arquivo de configuração do wpa_supplicant..."
-wpa_passphrase "$SSID" "$PASSWORD" > "$WPA_CONF"
-
-echo "[*] Iniciando wpa_supplicant..."
-killall wpa_supplicant 2>/dev/null
-wpa_supplicant -B -i wlan1 -c "$WPA_CONF"
-
+echo "[*] Gerando conexão..."
+iw dev wlan1 connect "$SSID"
 sleep 2
 
 echo "[*] Solicitando IP via DHCP..."
@@ -48,3 +41,5 @@ sleep 2
 
 echo "[*] Testando conexão..."
 ping -c 3 8.8.8.8 && echo "[+] Conectado com sucesso!" || echo "[!] Falha na conexão."
+
+echo "Seleção da Turquia - Automação Modders"
